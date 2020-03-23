b2a9a0418b007dc963667b66b76714d34bafd3cd
lejuuso <juuso.leppanen@helsinki.fi>
2020-03-23 13:09:07 +0200

    crud

--- src/main/java/academy/fi/finalproject/HelpWantedController.java
+++ src/main/java/academy/fi/finalproject/HelpWantedController.java
@@ -3,7 +3,10 @@ package academy.fi.finalproject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class HelpWantedController {
     @Autowired HelpWantedRepository repo;
@@ -20,5 +23,25 @@ public class HelpWantedController {
     public void delete(@PathVariable(name="id") Integer id){
         repo.deleteById(id);
     }
+
+    @PutMapping("/{id}")
+    public void update(@RequestBody HelpWanted h, @PathVariable(name = "id", required = true) int id){
+        Optional<HelpWanted> help = repo.findById(id);
+        HelpWanted hw = help.orElseThrow(RuntimeException::new);
+        hw.setHeadline(h.getHeadline());
+        hw.setName(h.getName());
+        hw.setPostcode(h.getPostcode());
+        hw.setEmail(h.getEmail());
+        hw.setDescription(h.getDescription());
+        hw.setGroceries(h.getGroceries());
+        hw.setChildCare(h.getChildCare());
+        hw.setDogOut(h.getDogOut());
+        hw.setOutdoorCompany(h.getOutdoorCompany());
+        hw.setTakingOutTrash(h.getTakingOutTrash());
+        hw.setOther(h.getOther());
+        repo.save(hw);
+
+    }
+
 
 }
